134c17ccbb7534f087b70a35669e8767133ba089
zotov88 <zotov_l88@mail.ru>
2022-05-10 21:30:20 +0300

    Add class Game and method play

---
+++ src/tictactoe/Game.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2022. http://devonline.academy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package tictactoe;
+
+import java.util.Random;
+
+/**
+ * @autor zotov_l88
+ */
+public class Game {
+
+    public void play() {
+        System.out.println("You can use numbers from 1 to 9 for the game");
+        final GameField gameField = new GameField();
+        dataPrinter.printStartField();
+        if (new Random().nextBoolean()) {
+            computerMove.step(gameField);
+            dataPrinter.printGameField(gameField);
+        }
+        while (true) {
+            userMove.step(gameField);
+            dataPrinter.printGameField(gameField);
+            if (winnerDetermination.isUserWin(gameField)) {
+                System.out.println("You win!\nGame Over");
+                return;
+            }
+            if (drawDetermination.isDraw(gameField)) {
+                System.out.println("It's draw!");
+                break;
+            }
+            computerMove.step(gameField);
+            dataPrinter.printGameField(gameField);
+            if (winnerDetermination.isComputerWin(gameField)) {
+                System.out.println("You loose!");
+                break;
+            }
+            if (drawDetermination.isDraw(gameField)) {
+                System.out.println("It's draw!");
+                break;
+            }
+        }
+        System.out.println("Game Over");
+    }
+}
