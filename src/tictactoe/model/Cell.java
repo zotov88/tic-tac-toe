ea0bd03fbac5d539b2ecb28c74f10215d51f6bae
zotov88 <zotov_l88@mail.ru>
2022-10-23 19:22:29 +0300

    Add model classes

---
+++ src/tictactoe/model/Cell.java
@@ -0,0 +1,23 @@
+package tictactoe.model;
+
+/**
+ * @author zotov_l88
+ */
+public class Cell {
+
+    private final int row;
+    private final int col;
+
+    public Cell(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+}
--- src/tictactoe/model/GameTable.java
+++ src/tictactoe/model/GameTable.java
@@ -4,4 +4,22 @@ package tictactoe.model;
  * @author zotov_l88
  */
 public class GameTable {
+
+    private final char[][] table = {
+        {' ', ' ', ' '},
+        {' ', ' ', ' '},
+        {' ', ' ', ' '}
+    };
+
+    public boolean isEmpty(final Cell cell) {
+        return table[cell.getRow()][cell.getCol()] == ' ';
+    }
+
+    public char getSign(final Cell cell) {
+        return table[cell.getRow()][cell.getCol()];
+    }
+
+    public void setSign(final Cell cell, final char sign) {
+        table[cell.getRow()][cell.getCol()] = sign;
+    }
 }
