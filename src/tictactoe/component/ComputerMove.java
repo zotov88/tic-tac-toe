7dedca950d3e6b494afa30f67be7cd3cdf164cad
zotov88 <zotov_l88@mail.ru>
2022-10-24 14:01:08 +0300

    Implement random computer move

--- src/tictactoe/component/ComputerMove.java
+++ src/tictactoe/component/ComputerMove.java
@@ -1,13 +1,25 @@
 package tictactoe.component;
 
+import tictactoe.model.Cell;
 import tictactoe.model.GameTable;
+
+import java.util.Random;
 
 /**
  * @author zotov_l88
  */
 public class ComputerMove {
 
-    public void make(GameTable gameTable) {
-
+    public void make(final GameTable gameTable) {
+        final Random random = new Random();
+        while (true) {
+            int row = random.nextInt(3);
+            int col = random.nextInt(3);
+            final Cell cell = new Cell(row, col);
+            if (gameTable.isEmpty(cell)) {
+                gameTable.setSign(cell, '0');
+                return;
+            }
+        }
     }
 }
